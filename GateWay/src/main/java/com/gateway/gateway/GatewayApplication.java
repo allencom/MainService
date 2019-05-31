80600832ae498f3a56222ff0e98d0ddc482d4c12
Huang Hua <350521722@qq.com>
2019-05-31 17:41:10 +0800

    0531

--- GateWay/src/main/java/com/gateway/gateway/GatewayApplication.java
+++ GateWay/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
